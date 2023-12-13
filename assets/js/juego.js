46f753a1fea30ddda5b57297169fb15cd81c5303
David García <ccristhiangarcia@gmail.com>
2023-12-12 20:11:11 -0500

    (Práctica): Crear baraja de cartas

---
+++ assets/js/juego.js
@@ -0,0 +1,34 @@
+/**
+ * 2C = Two of Clubs (Tréboles)
+ * 2D = Two of Diamonds (Diamantes)
+ * 2H = Two of Hearts (Corazones)
+ * 2S = Two of Spades (Espadas)
+ */
+
+let deck = [];
+let tipos = ['C', 'D', 'H', 'S'];
+let especiales = ['A', 'J', 'Q', 'K'];
+
+const shuffle = (array) => {
+    return array.sort(() => Math.random() - 0.5);
+}
+
+const crearDeck = () => {
+    for (let i = 2; i <= 10; i++) {
+        for (let tipo of tipos) {
+            deck.push(i + tipo);
+        }
+    }
+
+    for (let tipo of tipos) {
+        for (let esp of especiales) {
+            deck.push(tipo + esp);
+        }
+    }
+
+    console.log(deck);
+    shuffle(deck);
+    console.log(deck);
+}
+
+crearDeck();
