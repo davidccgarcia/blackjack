b97406137fa3df2a91524640695125250fa5828f
David García <ccristhiangarcia@gmail.com>
2023-12-23 22:05:53 -0500

    Valor de cada carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -49,4 +49,14 @@ const pedirCarta = () => {
     return carta;
 }
 
-pedirCarta();
+// pedirCarta();
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+
+    return ( isNaN( valor ) ) ? 
+            ( valor === 'A' ) ? 11 : 10
+            : valor * 1;
+}
+
+const valor = valorCarta(pedirCarta());
+console.log({ valor });
