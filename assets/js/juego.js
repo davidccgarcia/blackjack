040c2dba7a6509125a8058ac5e8a1a37d96d2b12
David García <ccristhiangarcia@gmail.com>
2023-12-25 11:55:40 -0500

    Turno de la computadora

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -14,7 +14,9 @@ let puntosJugador = 0,
 
 // Referencias HTML
 const btnPedir = document.querySelector('#btnPedir');
+const btnDetener = document.querySelector('#btnDetener');
 const divJugadorCartas = document.querySelector('#jugador-cartas');
+const divComputadoraCartas = document.querySelector('#computadora-cartas');
 
 const puntosHTML = document.querySelectorAll('small');
 
@@ -62,6 +64,29 @@ const valorCarta = (carta) => {
             : valor * 1;
 }
 
+// Esta función le permite mostrar las cartas en el HTML a la computadora
+const turnoComputadora = (puntosMinimos) => {
+    do {
+        const carta = pedirCarta();
+        puntosComputadora = puntosComputadora + valorCarta(carta);
+
+        puntosHTML[1].innerText = puntosComputadora;
+
+        // <img class="carta" src="assets/cartas/2C.png" alt="carta">
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cartas/${ carta }.png`;
+        imgCarta.classList.add('carta');
+        imgCarta.alt = 'carta';
+        divComputadoraCartas.append(imgCarta);
+
+        if (puntosMinimos > 21) {
+            break;
+        }
+
+    } while ( ( puntosComputadora < puntosMinimos ) && ( puntosMinimos <= 21 ) )
+        
+}
+
 // Eventos
 btnPedir.addEventListener('click', () => {
     const carta = pedirCarta();
@@ -79,8 +104,18 @@ btnPedir.addEventListener('click', () => {
     if (puntosJugador > 21) {
         console.warn('Lo siento mucho, perdiste!');
         btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora(puntosJugador);
     } else if (puntosJugador === 21) {
         console.warn('21, Genial!');
         btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora(puntosJugador);
     }
+});
+
+btnDetener.addEventListener('click', () => {
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoComputadora(puntosJugador);
 });
