cb74a062d593f166810e8a87d4531fdf8c602ab2
David García <ccristhiangarcia@gmail.com>
2023-12-26 08:20:53 -0500

    Optimizaciones tercera parte

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -33,9 +33,17 @@
     const inicialiazarJuego = ( numJugadores = 2 ) => {
         deck = crearDeck();
 
+        puntosJugadores = [];
+
         for (let i = 0; i < numJugadores; i++) {
             puntosJugadores.push(0);
         }
+
+        puntosHTML.forEach( elem => elem.innerText = 0);
+        divCartasJugadores.forEach( elem => elem.innerHTML = '');
+
+        btnPedir.disabled = false;
+        btnDetener.disabled = false;
     }
 
     // Esta función crea un nuevo deck
@@ -94,22 +102,10 @@
         divCartasJugadores[turno].append( imgCarta );
     }
 
-    // Esta función le permite mostrar las cartas en el HTML a la computadora
-    const turnoComputadora = ( puntosMinimos ) => {
+    // Esta función me permite determinar quien es el ganador del juego
+    const determinarGanador = () => {
 
-        let puntosComputadora = 0;
-
-        do {
-
-            const carta = pedirCarta();
-            puntosComputadora = acumularPuntos( carta, puntosJugadores.length - 1 );
-            crearCarta( carta, puntosJugadores.length - 1 );
-
-            if (puntosMinimos > 21) {
-                break;
-            }
-
-        } while ( ( puntosComputadora < puntosMinimos ) && ( puntosMinimos <= 21 ) )
+        let [ puntosMinimos, puntosComputadora ] = puntosJugadores;
 
         setTimeout(() => {
             if (puntosComputadora === puntosMinimos) {
@@ -121,8 +117,23 @@
             } else {
                 alert('Computadora gana!');
             }
-        }, 20);
-            
+        }, 30);
+    }
+
+    // Esta función le permite mostrar las cartas en el HTML a la computadora
+    const turnoComputadora = ( puntosMinimos ) => {
+
+        let puntosComputadora = 0;
+
+        do {
+
+            const carta = pedirCarta();
+            puntosComputadora = acumularPuntos( carta, puntosJugadores.length - 1 );
+            crearCarta( carta, puntosJugadores.length - 1 );
+
+        } while ( ( puntosComputadora < puntosMinimos ) && ( puntosMinimos <= 21 ) )
+
+        determinarGanador();
     }
 
     // Eventos
@@ -152,24 +163,7 @@
     });
 
     btnNuevo.addEventListener('click', () => {
-        console.clear();
-
         inicialiazarJuego();
-        /* deck = [];
-        crearDeck(); */
-
-        /* puntosHTML.forEach((punto) => {
-            punto.innerText = 0;
-        }); */
-
-        /* puntosJugador     = 0;
-        puntosComputadora = 0; */
-
-        /* divJugadorCartas.innerHTML     = '';
-        divComputadoraCartas.innerHTML = '';
-
-        btnPedir.disabled = false;
-        btnDetener.disabled = false; */
     });
 
 })();
